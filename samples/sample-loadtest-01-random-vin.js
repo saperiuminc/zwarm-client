89dff670467460f78c279864d4126e2dae35b1a0
Jerome Davadilla <jerome@saperium.com>
2023-05-31 06:19:35 -0500

    Fix error handling on failed conn

--- samples/sample-loadtest-01-random-vin.js
+++ samples/sample-loadtest-01-random-vin.js
@@ -3,5 +3,5 @@ it('Test 1', async() => {
     await page.goto('http://randomvin.com/getvin.php?type=real');
     const vin = await page.evaluate(() => document.body.innerText);
     await $log(`${$iteration}(${$VU}): ${vin}`);
-    return await sleep(1000);
+    return await $sleep(1000);
 });
